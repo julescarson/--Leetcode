bf77ba2662367bbeb2c11416b844354cb3826607
julescarson <metamoniker@gmail.com>
2024-09-04 01:47:41 -0400

    added python version
    trying to refresh my syntax

---
+++ python/buySellStock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max = 0
+        min = prices[0]
+
+        for i in range(len(prices)):
+            if (prices[i] < min):
+                min = prices[i]
+            elif (prices[i]-min > max):
+                max = prices[i]
+        return max
